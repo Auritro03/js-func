d95cd5f33495501169c44239c1938930e06c5e76
Easa Yeasfi Bin Islam <oritrobinislam@gmail.com>
2024-09-05 14:42:34 +0600

    parameter done

--- jj.js
+++ jj.js
@@ -1,1 +1,1 @@
-SDad
+SDad 
--- parameter.js
+++ parameter.js
@@ -1,2 +1,14 @@
+function pixel(money){
+    console.log('taka dilam ', money)
 
-asdaewdasdAW
+console.log('PIXEL kinlam');
+}
+
+//pixel(33300);
+
+function add(a,b,c,d,e){
+    console.log(a, b, c, d, e);
+    var add = a + b + c + d + e;
+    console.log('add');
+}
+add(2,3,4,4,5);
